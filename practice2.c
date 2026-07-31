ba158442d6346565df2d35d6b34378f1c88bbb9a
MD. AYNAL HAQUE <mdaynal01913@gmail.com>
2026-07-31 20:23:56 +0600

    Add files via upload

---
+++ practice2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>    //for input output
+int main (){           //int main is called main function
+
+    printf(" *   *   *\n"); //\n break the line
+    printf("   * * * \n");
+    printf("    ***\n");
+    printf("   * * *\n");
+    printf(" *   *   *\n");
+
+    
+
+
+
+    return 0;
+}
