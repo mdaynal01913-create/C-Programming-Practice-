0014063d416d442a52dc95b00b38d993dab40172
MD. AYNAL HAQUE <mdaynal01913@gmail.com>
2026-08-01 19:41:59 +0600

    Add files via upload

---
+++ introduction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+
+    printf("\n********************************************\n\n");
+
+    printf("NAME          MD. AYNAL HAQUE \n");
+    printf("DEPARTMENT    COMPUTER SCIENCE AND ENGINEERING\n");
+    printf("SEMESTER      FIRST \n");
+    printf("COURSE        C PROGRAMMING\n");
+    printf("UNIVERSITY    PABNA UNIVESITY OF DCIENCE AND TECHNOLOGY\n\n");
+
+    printf("*********************************************");
+
+    return 0;
+}
