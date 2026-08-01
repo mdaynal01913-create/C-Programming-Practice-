30b3fb9ea9672b4983ecef7a0edfea05ed8f52e8
MD. AYNAL HAQUE <mdaynal01913@gmail.com>
2026-08-01 20:08:53 +0600

    Add files via upload

---
+++ practice3.c
@@ -0,0 +1,19 @@
+/* There are compilation errors in the program below. List all the errors and fix them.
+Also, give indentation and spacing to beautify the program and then compile and run.
+
+/*include <stdio.h> // header // file
+/* This program /* shows greetings */
+//int main( /* main */ function */
+//{ { print(Hello\n); printf(World" return 0; }/*
+
+#include <stdio.h>
+int main  (){
+    printf("hellow  ");
+    printf("world");
+
+
+
+
+
+    return 0;
+}
